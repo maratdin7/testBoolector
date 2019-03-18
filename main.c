2a1fae04e12945bab3f195971ba1774d6aeb8403
maratdin7 <maratdin7@gmail.com>
2019-03-19 02:53:39 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "boolector.h"
+#include <assert.h>
+#include <limits.h>
+#include "boolector.h"
+#include "btoropt.h"
+
+int main() {
+        int result;
+        BoolectorNode *array, *index1, *index2, *read1, *read2, *eq, *ne;
+        BoolectorSort sort_index, sort_elem, sort_array;
+        Btor *btor;
+        btor = boolector_new();
+    return 0;
+}
